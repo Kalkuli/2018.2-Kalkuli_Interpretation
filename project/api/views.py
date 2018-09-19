5a7b70a9b1c1f9f4e034905d836da728b2afd4cd
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-09-19 14:57:55 -0300

    Add utils file and refactor views.
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Saleh Nazih <saleh.nazih.dev@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request, Blueprint
-import re
-
+from project.api.utils import Interpreter
 
 interpret_blueprint = Blueprint('interpret', __name__)
 
@@ -14,16 +13,9 @@ def interpret():
 
     raw_text = data.get('raw_text')
     raw_text = raw_text.lower()
-    price_regex = re.compile('valor total r\$ \d+,\d{2}')
-    list_prices = price_regex.findall(raw_text)
 
-    date_regex = re.compile('\d{2}/\d{2}/\d{4}')
-    list_date = date_regex.findall(raw_text)
+    interpreter = Interpreter(raw_text)
 
-    cnpj_regex = re.compile('\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}')
-    list_cnpj = cnpj_regex.findall(raw_text)
-    return jsonify({
-        'list_prices': list_prices,
-        'list_date': list_date,
-        'list_cnpj': list_cnpj
-        }), 201
+    response = interpreter.interpret_text()
+
+    return jsonify(response), 201
