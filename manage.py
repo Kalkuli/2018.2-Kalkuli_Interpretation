2e1e3ecb7db7a167534e908f996a182a1e1a22c8
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-09-20 17:04:26 -0300

    Add coverage support

--- manage.py
+++ manage.py
@@ -1,6 +1,17 @@
 from flask.cli	import	FlaskGroup
 from project	import	create_app
 import unittest
+import coverage
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 
 app = create_app()
@@ -12,6 +23,21 @@ def test():
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     return 1
 
