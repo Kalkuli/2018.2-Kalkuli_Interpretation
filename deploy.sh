1832dba60a3517dd95858c2346a0b021dc3c37d6
Lucas Dutra <ldutra98@gmail.com>
2018-11-07 22:11:42 -0200

    Create homolog env
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Bernardo Henrique <bernardohrl.unb@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,27 @@ set -e
 set -u
 
 
-if [ $TRAVIS_PULL_REQUEST != "false" -o $TRAVIS_BRANCH != "master" ]
+if [ $TRAVIS_PULL_REQUEST = "false" -o $TRAVIS_BRANCH = "develop" ]
 then
-    echo "Skipping deployment on branch=$TRAVIS_BRANCH, PR=$TRAVIS_PULL_REQUEST"
+    echo "Deploy on homologation environment started"
+    pip3 install zappa
+    zappa update hom
+    echo "Deploy on homologation environment finished"
     exit 0;
 fi
+
+if [ $TRAVIS_PULL_REQUEST = "false" -o $TRAVIS_BRANCH = "master" ]
+then
+    echo "Deploy on production environment started"
+    pip3 install zappa
+    zappa update prod
+    echo "Deploy on production environment finished"
+    exit 0;
+fi
+
+
+echo "Skipping deployment on branch=$TRAVIS_BRANCH, PR=$TRAVIS_PULL_REQUEST"
+
 
 # Heroku Deployment
 # docker login -u _ -p "$HEROKU_TOKEN" registry.heroku.com
@@ -17,5 +33,3 @@ fi
 # heroku container:release web -a kalkuli-interpretation
 
 # Zappa Deployment
-pip3 install zappa
-zappa update dev
